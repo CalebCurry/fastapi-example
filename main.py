956d019b0ae3e09fb34500713847caeee75e0072
Caleb Curry <caleb@calebcurry.com>
2025-09-07 14:15:28 -0400

    cursor based with next

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import base64
 from contextlib import asynccontextmanager
 from datetime import datetime, timezone
+import json
 from fastapi import Depends, FastAPI, HTTPException, Query, Request
 from typing import Annotated, Generic, Optional, TypeVar
 
 from pydantic import BaseModel
-from sqlmodel import Field, SQLModel, Session, create_engine, func, select
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+# pyright: reportAssignmentType=none
+# pyright: reportArgumentType=none
 
 class Campaign(SQLModel, table=True):
-    campaign_id: int | None = Field(default=None, primary_key=True)
+    campaign_id: int = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     due_date: datetime | None = Field(default=None, index=True)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), index=True)
@@ -57,26 +61,38 @@ class Response(BaseModel, Generic[T]):
 class PaginatedResponse(BaseModel, Generic[T]):
       data: T
       next: Optional[str]
-      prev: Optional[str]
+      #prev: Optional[str]
+
+def encode_cursor(value):
+    raw = json.dumps({"id": value})
+    return base64.urlsafe_b64encode(raw.encode()).decode()
+
+def decode_cursor(cursor):
+    raw = base64.urlsafe_b64decode(cursor.encode()).decode()
+    payload = json.loads(raw)
+    return payload.get("id")
 
 @app.get("/campaigns", response_model=PaginatedResponse[list[Campaign]])
-async def read_campaigns(request: Request, session: SessionDep, offset: int = Query(0, ge=0), limit: int = Query(20, ge=1)):
+async def read_campaigns(request: Request, session: SessionDep, cursor: Optional[str] = Query(None), limit: int = Query(20, ge=1)):
+    cursor_id = 0
 
-    data = session.exec(select(Campaign).order_by(Campaign.campaign_id).offset(offset).limit(limit)).all() #type: ignore
+    if cursor:
+        cursor_id = decode_cursor(cursor)
+
+    data = session.exec(select(Campaign).order_by(Campaign.campaign_id).where(Campaign.campaign_id > cursor_id).limit(limit+1)).all()
 
     base_url = str(request.url).split('?')[0]
 
-    next_url = f"{base_url}?offset={offset+limit}&limit={limit}"
+    next_url = None
 
-    if offset > 0:
-        prev_url = f"{base_url}?offset={max(0, offset-limit)}&limit={limit}"
-    else:
-        prev_url = None
+    if len(data) > limit:
+        next_cursor = encode_cursor(data[:limit][-1].campaign_id)
+        next_url = f"{base_url}?cursor={next_cursor}&limit={limit}"
 
     return {
         "next": next_url,
-        "prev": prev_url,
-        "data": data
+        #"prev": prev_url,
+        "data": data[:limit]
     }
 
 @app.get("/campaigns/{id}", response_model=Response[Campaign])
@@ -86,7 +102,7 @@ async def read_campaign(id: int, session: SessionDep):
         raise HTTPException(status_code=404)
     return {"data": data}
 
-@app.post("/camapaigns", status_code=201, response_model=Response[Campaign])
+@app.post("/campaigns", status_code=201, response_model=Response[Campaign])
 async def create_campaign(campaign: CampaignCreate, session: SessionDep):
     db_campaign = Campaign.model_validate(campaign)
     session.add(db_campaign)
